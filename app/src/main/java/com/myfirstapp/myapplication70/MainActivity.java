6a33ba5600b6e29b0f9d54b77bbeac36436ffcb5
Dharmaraj <gdharmarajbtech@gmail.com>
2023-02-15 12:46:43 +0530

    Final commit

---
+++ app/src/main/java/com/myfirstapp/myapplication70/MainActivity.java
@@ -0,0 +1,16 @@
+package com.myfirstapp.myapplication70;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.os.Bundle;
+
+import com.myfirstapp.myapplication70.R;
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
